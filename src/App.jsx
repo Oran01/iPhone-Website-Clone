22de95d67b579290345ad598aa7aa184ff93359c
Oran Alster <117355063+Oran01@users.noreply.github.com>
2025-04-30 13:51:32 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
-import Features from "./components/features";
+import Features from "./components/Features";
 import HowItWork from "./components/HowItWork";
 import Footer from "./components/Footer";
 
